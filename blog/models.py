fe5aa7a5ac4edd924ed502fdee86cfa19caa40ff
M Awais <mawaishanif@hotmail.com>
2017-11-10 04:03:50 -0800

    Change the representation of Post model and and make fields required in Categories Model

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Categories(models.Model):
     category_name = models.CharField(max_length=50)
     category_url = models.CharField(max_length=60)
-    category_desc = models.TextField()
+    category_desc = models.TextField(blank=True)
 
     def __str__(self):
         return self.category_name
@@ -12,10 +12,10 @@ class Categories(models.Model):
 
 class Post(models.Model):
     post_title = models.CharField(max_length=200, blank=False)
-    post_content = models.TextField()
+    post_content = models.TextField(blank=True)
     post_url = models.CharField(max_length=250, blank=False)
     # Many to many field to categories.
     post_category = models.ManyToManyField(Categories)
 
     def __str__(self):
-        return self.post_url
+        return self.post_title
