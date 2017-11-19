4566f99b9a7a3acaeffa7be76927bd712323e5e5
M Awais <mawaishanif@hotmail.com>
2017-11-18 19:19:54 -0800

    Change the url fields to SlugField

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 
 class Categories(models.Model):
     category_name = models.CharField(max_length=50)
-    category_url = models.SlugField(max_length=60)
+    category_url = models.SlugField(max_length=60, unique=True)
     category_desc = models.TextField(blank=True)
 
     def __str__(self):
@@ -14,7 +14,7 @@ class Categories(models.Model):
 class Post(models.Model):
     post_title = models.CharField(max_length=200, blank=False)
     post_content = models.TextField(blank=True)
-    post_url = models.SlugField(max_length=250)
+    post_url = models.SlugField(max_length=250, unique=True)
     # Many to many field to categories.
     post_category = models.ManyToManyField(Categories, blank=True)
 
