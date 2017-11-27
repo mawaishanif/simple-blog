ff49447d1777c349a6eee868f16aea8fd3f73c08
M Awais <mawaishanif@hotmail.com>
2017-11-27 03:19:17 -0800

    Add url for blog homepage

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url  # Django url configuration class.
-from . import views  # Importing blog app views
+from blog.views import BlogHomeView  # Importing blog app views
 
 
 app_name = 'blog'
-urlpatterns = []
+urlpatterns = [
+    url(r'^$', BlogHomeView.as_view(), name='blog_home'),
+]
