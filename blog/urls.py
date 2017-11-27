4ad765b9a90f0dd15275f5a2974b1d9b8344f49e
M Awais <mawaishanif@hotmail.com>
2017-11-27 04:11:36 -0800

    Add full archive and for year

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url  # Django url configuration class.
 from blog.views import BlogHomeView  # Importing blog app views
+from django.views.generic.dates import ArchiveIndexView
+from blog.views import PostYearArchiveView
+from blog.models import Post
 
 
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', BlogHomeView.as_view(), name='blog_home'),
+    url(r'^archive/$', ArchiveIndexView.as_view(model=Post, date_field='creation_date'), name='post_archive'),
+    url(r'^(?P<year>[0-9]{4})/$', PostYearArchiveView.as_view(), name="article_year_archive"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from .models import Post
+from django.views.generic.dates import YearArchiveView
 
 
 class BlogHomeView(TemplateView):
@@ -13,3 +14,9 @@ class BlogHomeView(TemplateView):
         for post in context['latest_articles']:
             setattr(post, 'categorie', post.category.all()[0])
         return context
+
+
+class PostYearArchiveView(YearArchiveView):
+    queryset = Post.objects.all()
+    date_field = "creation_date"
+    make_object_list = True
