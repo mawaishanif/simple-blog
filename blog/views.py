1bb9446ed8f66054d3c022ac8a1f8fb63e5af4ca
M Awais <mawaishanif@hotmail.com>
2017-11-27 03:18:46 -0800

    Add function for blog homepage

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import Post
 
-# Create your views here.
+
+class BlogHomeView(TemplateView):
+    template_name = 'blog/blog-home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogHomeView, self).get_context_data(**kwargs)
+        context['latest_articles'] = Post.objects.all()[:5]
+        categories = {}
+        for post in context['latest_articles']:
+            setattr(post, 'categorie', post.category.all()[0])
+        return context
